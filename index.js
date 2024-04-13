de0331a90ff4456e15aa1fe818a0f4c5e98350aa
Al_Iraq1 <152442043+q0so@users.noreply.github.com>
2024-04-13 10:18:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const { Telegraf } = require('telegraf');
 
+const http = require('http');
+
+const requestHandler = (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World!\n');
+};
+
+const server = http.createServer(requestHandler);
+
+const port = 3000;
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Something went wrong: ', err);
+  }
+
+  console.log(`Server is listening on port ${port}`);
+});
+
 const bot = new Telegraf('6668072595:AAF5uTZeNcsX0dEWfWecYvt2tFgU_Wje2aA');
 
-bot.start((ctx) => ctx.reply('Welcome! Send me a file.'));
+bot.start((ctx) => ctx.reply('Welcome! Send me a file. \n Hi \n d'));
 
 bot.on('document', async (ctx) => {
   try {
